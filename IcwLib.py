f79b08709ee87d22e7c3dca190925ef0ae2ba798
tymancjo <tomasz.tomanek@gmail.com>
2017-03-11 21:40:24 +0100

    added local files

---
+++ IcwLib.py
@@ -0,0 +1,128 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Defining fuction to draw copperbar shape
+def drawCuShape(barGeometry):
+
+    numberOfSegments = barGeometry.shape[0]
+    #checking for max bar height in all segments
+    #and set the center of bar position y
+    centerY = np.max(barGeometry, axis=0)[0]/2
+
+    currentX = 0
+    plt.figure(figsize=(12,4))
+
+    for i in range(0,numberOfSegments,1):
+
+
+        # Ploting main coppershape of the segment
+        currentY = centerY-barGeometry[i, 0]/2
+        segment = plt.Rectangle((currentX, currentY),barGeometry[i, 2]\
+        ,barGeometry[i, 0], fc='orange', linestyle='dashed', edgecolor="red")
+        plt.gca().add_patch(segment)
+
+        #adding text
+        plt.text(currentX + barGeometry[i, 2]/2 -5 , \
+        currentY + barGeometry[i, 0]/2 -3 ,"["+str(i+1)+"]")
+
+        if barGeometry[i, 3] != 0:
+            # Drawing the hole in segment
+            hole = plt.Rectangle((currentX, \
+            currentY+(barGeometry[i, 0]/2)-(barGeometry[i, 3]/2)),\
+            barGeometry[i, 2],barGeometry[i, 3], fc='w')
+            plt.gca().add_patch(hole)
+
+        currentX += barGeometry[i, 2]
+
+    #print("Copperbar geometry visualisation")
+    plt.axis('scaled')
+    #plt.show()
+
+
+def crossSection(barSegmentArray):
+    #cross section = (H-otw)*w
+    return ((barSegmentArray[0] - barSegmentArray[3])*barSegmentArray[1])*1e-6
+
+# Calulation of power losses
+# the idea is to do it segment by segment applaying
+# to subrutine the cross section definition
+def powerLosses(xSec,Irms,temp,cond20C, thermRcoef):
+
+    #analysis of xSec array
+    #getting data from array
+    heightBar = xSec[0]*1e-3
+    lenghtBar = xSec[2]*1e-3
+    thickBar  = xSec[1]*1e-3
+    cutoutBar = xSec[3]*1e-3
+
+    condAtTemp = cond20C / (1 + thermRcoef*(temp - 20))
+
+    activeCrossSection = crossSection(xSec)
+    Rdc = lenghtBar/(activeCrossSection * condAtTemp)
+    return 1*Rdc * Irms**2
+
+def copperCp(temperatureCu):
+    return 423.28-45.089*np.exp(-1*temperatureCu/192.82)
+
+def htc(temp, tempAmb, initialHTC):
+    return 0.1*initialHTC*(temp - tempAmb)**0.25
+
+
+def generateTHermalConductance(barGeometry, thermalCOnduction):
+    numberOfSegments = barGeometry.shape[0]
+    thermG = np.zeros(numberOfSegments -1)
+
+    for i in range(0,numberOfSegments -1, 1):
+        #half lenght of this section thermal resistance
+        thR1 = 0.5*barGeometry[i][2]*1e-3/ \
+        (crossSection(barGeometry[i])*thermalCOnduction)
+        #half lenght of next section thermal resistance
+        thR1 += 0.5*barGeometry[i+1][2]*1e-3/ \
+        (crossSection(barGeometry[i+1])*thermalCOnduction)
+        thermG[i] = 1/thR1
+
+    return thermG
+
+#Defining function for temperature rise calculations
+def getTempDistr(barGeometry, Irms, timeStep, startTemp,\
+                ambientTemp, density, Cp, baseHTC, thermG, emmisivity):
+
+    numberOfSegments = barGeometry.shape[0]
+    segmentTemperatureRise = np.zeros(numberOfSegments)
+
+    # First loop over segments - calculating temp rise from srcs
+    # minus convection and conduction from previous timestep tepratures
+    for i in range (0, numberOfSegments, 1):
+        #getting data from array
+        heightBar = barGeometry[i][0]*1e-3
+        lenghtBar = barGeometry[i][2]*1e-3
+        thickBar  = barGeometry[i][1]*1e-3
+        cutoutBar = barGeometry[i][3]*1e-3
+
+        segmentVolume = (heightBar - cutoutBar) * thickBar * lenghtBar
+        segmentMass = segmentVolume * density
+        barArea = lenghtBar * (2 * ((heightBar - cutoutBar)+ thickBar))
+
+        #Thermal conduction based on the previous step tmeperatures
+        if i>0 and i<numberOfSegments-1:
+            thermalCOnduction = ((startTemp[i]-startTemp[i-1])*thermG[i-1]+\
+            (startTemp[i]-startTemp[i+1])*thermG[i])
+        elif i==0:
+            thermalCOnduction = ((startTemp[i]-startTemp[i+1]))*thermG[i]
+        else:
+            thermalCOnduction = ((startTemp[i]-startTemp[i-1]))*thermG[i-1]
+
+        stBoltzConst = 5.6703e-8
+        thermalRadiation = stBoltzConst * barArea * emmisivity\
+        * ((startTemp[i]+273.15)**4-(ambientTemp+273.15)**4)
+
+        thermalConvection = htc(startTemp[i],ambientTemp,baseHTC) \
+        * barArea*(startTemp[i]-ambientTemp)
+
+        segmentTemperatureRise[i] = (powerLosses(barGeometry[i], \
+        Irms, startTemp[i], 58e6, 3.9e-3) \
+        - thermalConvection\
+        - thermalCOnduction - thermalRadiation) \
+        * timeStep / (segmentMass * copperCp(startTemp[i]))
+
+    return segmentTemperatureRise
---
+++ IcwThermal_AlongSingleBar.py
@@ -0,0 +1,65 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from IcwLib import *
+
+# Definig the bar as segments of geometry
+# the formula is for each ssegment
+# [[SegmentHeight, SegmentThickness, SegmentLenght, CutoutHeight]]
+# Cutout is assumed to be along entire segment lenght
+
+# Single bar 3 segments 2 of them with holes (beggining and end) definition
+copperBarGeometry = np.array([[100,10,50,0],[30,10,10,0],[30,10,10,10],\
+[30,10,145,0],[30,10,10,10],[30,10,145,0],\
+[30,10,10,10],[30,10,10,0],[100,10,50,0]])
+# end of Bar geometry definition
+# print(copperBarGeometry)
+
+
+# Defining current load
+def currentIcw(time):
+    if time <= 3:
+        return 25e3
+    else:
+        return 0
+
+
+# Loop for transient analysis
+endTime = 60*10
+numberOfSamples = 10*endTime
+ambientTemp = 24
+
+numberOfSegments = copperBarGeometry.shape[0]
+
+thermalGarray = generateTHermalConductance(copperBarGeometry, 401)
+print(thermalGarray)
+
+deltaTime = float(endTime) / float(numberOfSamples)
+
+temperatures = np.ones((numberOfSamples, numberOfSegments))*ambientTemp
+timeTable = np.zeros(numberOfSamples)
+
+for time in range(1,numberOfSamples,1):
+        currentTime = time * deltaTime
+
+        timeTable[time] = currentTime
+
+        temperatures[time] = temperatures[time-1]+ \
+        getTempDistr(copperBarGeometry,\
+        currentIcw(currentTime), deltaTime, temperatures[time -1] ,\
+        ambientTemp, 8920, 385, 100,thermalGarray, 0.75)
+
+
+plt.ylabel('Temp Rise [K]')
+plt.xlabel('time [s]')
+plt.grid(1)
+
+
+
+for i in range(0,numberOfSegments):
+    plt.plot(timeTable, np.array(temperatures)[:,i],\
+    label="["+str(i+1)+"]")
+
+# Place a legend to the right of this smaller subplot.
+plt.legend(bbox_to_anchor=(0.75, 0.95), loc=2, borderaxespad=0.)
+drawCuShape(copperBarGeometry)
+plt.show()
